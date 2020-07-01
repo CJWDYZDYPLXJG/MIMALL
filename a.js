c8cc9420a56f45a0c9f933446807e31ad05d12ce
CJWDYZDYPLXJG <fanxiaohaohaha@qq.com>
2020-07-01 11:07:31 +0800

    mi

---
+++ a.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-01 10:33:59
+ * @LastEditTime: 2020-07-01 10:34:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \MIMALL\a.js
+ */
+var a = 1;
