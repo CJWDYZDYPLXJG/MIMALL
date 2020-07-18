59b6dfb9a899f201183b87519cda98a273d11228
CJWDYZDYPLXJG <fanxiaohaohaha@qq.com>
2020-07-18 22:01:05 +0800

    2020年7月18日 22:01:05

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,18 @@
-
 <template>
     <div>
-        
+        <service-bar>
+            
+        </service-bar>
     </div>
-    
+
 </template>
 <script>
+    import ServiceBar from './../components/ServiceBar'
 
-export default {
-    name:'index'
-}
+    export default {
+        name: 'index',
+        components: {
+            ServiceBar
+        }
+    }
 </script>
